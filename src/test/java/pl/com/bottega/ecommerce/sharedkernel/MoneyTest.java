d52b54c5ea36a3545c83a9214868ad9a961fe7a1
Jan Godziatkowski <ph.godzio@gmail.com>
2015-03-20 10:20:26 +0100

    Refaktoryzacja klasy testów Money
    Refaktoryzacja testow aby spelnialy konwencje - uzycie adnotacji @Before

--- src/test/java/pl/com/bottega/ecommerce/sharedkernel/MoneyTest.java
+++ src/test/java/pl/com/bottega/ecommerce/sharedkernel/MoneyTest.java
@@ -8,6 +8,7 @@ package pl.com.bottega.ecommerce.sharedkernel;
 import static org.hamcrest.CoreMatchers.is;
 import org.junit.Test;
 import static org.junit.Assert.*;
+import org.junit.Before;
 
 /**
  *
@@ -15,13 +16,24 @@ import static org.junit.Assert.*;
  */
 public class MoneyTest {
 
-    private final Money first = new Money(20.00, "USD");
-    private final Money second = new Money(20.00, "USD");
-    private final Money third = new Money(10.00, "USD");
-    private final Money fourth = new Money(20.00, "PLN");
-    private final Money fifth = new Money(10.00, "PLN");
-    private final Money sixth = new Money(0.00, "USD");
-    private final Money minus = new Money(-10.00, "USD");
+    private Money first;
+    private Money second;
+    private Money third;
+    private Money fourth;
+    private Money fifth;
+    private Money sixth;
+    private Money minus;
+
+    @Before
+    public void onStart() {
+        first = new Money(20.00, "USD");
+        second = new Money(20.00, "USD");
+        third = new Money(10.00, "USD");
+        fourth = new Money(20.00, "PLN");
+        fifth = new Money(10.00, "PLN");
+        sixth = new Money(0.00, "USD");
+        minus = new Money(-10.00, "USD");
+    }
 
     @Test
     public void money_equals_twoElementsWithBothValuesDifferent_expectedFalse() {
@@ -107,11 +119,12 @@ public class MoneyTest {
     public void money_subtract_missmatchedCurrency_expectedException() {
         third.subtract(fourth);
     }
-    
+
     @Test
     public void money_add_thirdAddThirdEqualsFirst_expectedTrue() {
         assertThat(third.add(third), is(first));
     }
+
     @Test
     public void money_add_sixthEqualZeroAddThird_expectedTrue() {
         assertThat(sixth.add(third), is(third));
@@ -126,6 +139,5 @@ public class MoneyTest {
     public void money_add_missmatchedCurrency_expectedException() {
         third.add(fourth);
     }
-    
-    
+
 }
